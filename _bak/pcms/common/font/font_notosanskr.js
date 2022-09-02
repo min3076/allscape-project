c63517067fbb68783dac6f68b1f66e4c89a8d746
ju <vial222@gmail.com>
2022-09-02 15:20:11 +0900

    first0901 commit

--- _bak/pcms/common/font/font_notosanskr.js
+++
@@ -1,9 +0,0 @@
-
-(function() {
-if ( OS_ANDROID || OS_IOS ) return;
-if ( idvUserinfo.MSIE && parseInt( idvUserinfo.MSIE ) < 9 ) {
-    document.write( '<link type="text/css" rel="stylesheet" href="/pcms/common/font/font_notosanskr-eot.css"/>' );
-} else {
-    document.write( '<link type="text/css" rel="stylesheet" href="/pcms/common/font/font_notosanskr.css"/>' );
-}
-})();
